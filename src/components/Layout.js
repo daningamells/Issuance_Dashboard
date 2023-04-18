8824248b2a6aca30e696f2b222a6542fcbb75e2b
Daniel Ingamells <dan_ingamells@hotmail.co.uk>
2023-04-18 12:15:59 +0100

    added GTM

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,19 +2,33 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import Assumptions from './pages/Assumptions';
+import Apr from './pages/Apr';
 import Emissions from './pages/Emissions';
 import Simulations from './pages/Simulations';
 import Parameters from './pages/Parameters';
-
 import DrawerLayout from "./components/DrawerLayout";
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+    gtmId: 'GTM-N6NTB5C',
+}
+
+TagManager.initialize(tagManagerArgs)
+
 
 export default function Layout({children}) {
   return (
     <HelmetProvider>
-    <Router>
+    <Router basename="/shm-node-reward-calculator">
       <Helmet>
-        <title>Shardeum Issuance</title>
-        <meta name="description" content="Shardeum Issuance"/>
+        <title>Shardeum Node reward Calculator</title>
+        <meta name="description" content="Shardeum node reward calculator (initial release) is live which will allow validators to run node reward simulations and calculate the node income. Check it out today."/>
+        <meta property="og:title" content="Shardeum Node reward Calculator"/>
+        <meta property="og:site_name" content="Shardeum"/>
+        <meta property="og:url" content="https://shardeum.org/"/>
+        <meta property="og:description" content="Shardeum node reward calculator (initial release) is live which will allow validators to run node reward simulations and calculate the node income. Check it out today."/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:image" content="https://shardeum.org/explore/wp-content/uploads/2023/04/Shardeum-Node-Reward-Calculator.jpg"/>
         <link rel="icon" href="/favicon.ico"/>
       </Helmet>
       <main className="main py-5 px-5 md:px-20 ml-auto mr-auto  md:max-w-[75rem]">
@@ -23,6 +37,7 @@ export default function Layout({children}) {
           <Routes>
               <Route exact path='/' element={<Assumptions/>}/>
               <Route path='/Emissions' element={<Emissions/>}/>
+              <Route path='/Apy' element={<Apr/>}/>
               <Route path='/Parameters' element={<Parameters/>}/>
               <Route path='/Simulations' element={<Simulations/>}/>
           </Routes>
